964075099a6ef8cdb05ed331df5f113488f37bb4
sey0203 <hok4070@gmail.com>
2025-03-18 17:24:04 +0900

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,15 +1,33 @@
+import random
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service #브라우저를 핸들링, 안넣으면 기본값
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium_stealth import stealth
+
+user_agents = [
+    # Add your list of user agents here
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+]
+
 
 @pytest.fixture(scope="function")
 def driver():
-    #크롬 옵션 설정
-    chrome_options = Options()  #쿠팡은 자동화 크롤링 할때 많은 옵션수정이 필요.. 차단먹임
+    # 크롬 옵션 설정
 
-        # 1) User-Agent 변경
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) Firefox/91.0")
+    user_agent = random.choice(user_agents)
+
+    chrome_options = Options() #쿠팡이 자동화 크롤링 많은 옵션수정이 필요하다.. 
+    # 1) User-Agent 변경
+    chrome_options.add_argument(f'user-agent={user_agent}')
+
+    
     # 2) SSL 인증서 에러 무시
     chrome_options.add_argument("--ignore-certificate-errors")
     chrome_options.add_argument("--ignore-ssl-errors")
@@ -24,21 +42,41 @@ def driver():
     # "AutomationControlled" 자체가 표기되지 않도록 한다.
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 
-    # 5) 디버그 로깅 줄이기 (선택)
-    # chrome_options.add_argument("--log-level=3") 
-
     # 6) Sandbox나 DevShm 사이즈 문제 우회 (리눅스 환경에서 발생 가능)
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-dev-shm-usage")    
+#추가
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--start-maximized')
+    chrome_options.add_argument('--disable-popup-blocking')
 
-    #드라이버 객체 생성
+
+   
+    # 드라이버 객체 생성
     driver = webdriver.Chrome(service=Service(), options=chrome_options)
+        # instantiate a Chrome browser and add the options
+    #추가
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     driver.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": {"Referer": "https://www.coupang.com/"}})
+    driver.execute_cdp_cmd("Network.clearBrowserCache", {})
 
-    #대기시간 설정
+    # Step 4: Scrape using Stealth
+#enable stealth mode
+#추가
+    stealth(driver,
+        languages=["ko-KR", "ko"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+        )
+
+    driver.delete_all_cookies()
+    #  대기시간 설정
     driver.implicitly_wait(5)
 
-    yield driver
+    yield driver 
 
-    #테스트가 끝나면 드라이버 종료
+    # 테스트가 끝나면 드라이버 종료
     driver.quit()
