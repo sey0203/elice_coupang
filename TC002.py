303042b511a94d9056a2748408384d5171d767fb
sey0203 <hok4070@gmail.com>
2025-03-18 17:47:30 +0900

    Update TC002.py

--- TC002.py
+++ TC002.py
@@ -8,25 +8,28 @@ from selenium.webdriver.common.keys import Keys
 # TC001 장바구니 담기 기능 테스트
 
 
-class ProductPage:
-    ITEM_XPATH = "//form//ul/li[1]//a"
+class CartPage:
+    ITEM_XPATH = "//a[contains(@class, 'search-product-link')"
+    CART_BUTTON_XPATH = "//button[contains(text()='장바구니 담기')]"
+    MOVE_CART_BUTTON_CLASSNAME = "cart more"
 
     def __init__(self, driver: WebDriver):
         self.driver = driver
+
 
     def select_first_item(self):
         wait = ws(self.driver, 10)
-        first_item = wait.until(EC.element_to_be_clickable((By.XPATH, self.ITEM_XPATH)))
-        first_item.click()
+        first_item = wait.until(EC.presence_of_all_elements_located((By.XPATH, self.ITEM_XPATH)))
+        first_item[0].click()
 
-
-class Add_Cart:
-    CART_BUTTON_XPATH = "//button[contains(text()='장바구니 담기')]"
-
-    def __init__(self, driver: WebDriver):
-        self.driver = driver
 
     def add_to_cart(self):
         wait = ws(self.driver, 10)
         cart_button = wait.until(EC.element_to_be_clickable((By.XPATH, self.CART_BUTTON_XPATH)))
+        cart_button.click()
+
+
+    def move_to_cart(self):
+        wait = ws(self.driver, 10)
+        cart_button = wait.until(EC.presence_of_element_located((By.CLASS_NAME, self.MOVE_CART_BUTTON_CLASSNAME)))
         cart_button.click()
